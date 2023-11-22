a09f9ddca93e38941008ae4181c60cc36e86c7e2
joaoprfreitas <joaoprfreitas23@gmail.com>
2023-11-22 14:36:49 -0300

    update

--- main.c
+++ main.c
@@ -39,7 +39,6 @@ int manhattan(int x1, int y1, int z1, int x2, int y2, int z2) {
 
 int *createMatrix(int row, int column, int SEED);
 void printMatrix(int *matrix, int row, int column);
-void freeMatrix(int *matrix, int row);
 
 // TODO: tratar o caso de N n caber na memoria
 // TODO: tratar a seed nos nós
@@ -60,17 +59,18 @@ int main(int argc, char *argv[]) {
     T = atoi(argv[3]); // Número de threads
 
     if (N <= 0 || SEED <= 0 || T <= 0) {
-        printf("Invalid arguments, N and SEED must be positive numbers\n");
+        printf("Invalid arguments: N, SEED and T must be positive\n");
         return EXIT_FAILURE;
     }
 
     srand(SEED);
 
-    omp_set_num_threads(T);
+    omp_set_num_threads(T); // Define o número de threads
+    omp_set_nested(1); // Permite o uso de threads dentro de threads
 
-    int process_number, myrank;
-    int src = 0, dest, message_tag;
+    int process_number, myrank, src = 0, dest, message_tag;
 
+    // Inicializa o MPI
     MPI_Status status;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &process_number);
@@ -89,6 +89,8 @@ int main(int argc, char *argv[]) {
         MPI_Bcast(z, N*N, MPI_INT, src, MPI_COMM_WORLD);
 
 
+
+
         // printf("Distância de Manhattan mínima: %d (soma min: %d) e máxima: %d (soma max: %d).\n", min_manhattan, sum_min_manhattan, max_manhattan, sum_max_manhattan);
         // printf("Distância Euclidiana mínima: %.2lf (soma min: %.2lf) e máxima: %.2lf (soma max: %.2lf).\n", min_euclidean, sum_min_euclidean, max_euclidean, sum_max_euclidean);
   
@@ -99,12 +101,56 @@ int main(int argc, char *argv[]) {
         MPI_Bcast(y, N*N, MPI_INT, src, MPI_COMM_WORLD);
         MPI_Bcast(z, N*N, MPI_INT, src, MPI_COMM_WORLD);
 
-    }
+        // receive inicio e fim do for
+        int start, end;
+
+        // obtém os maximos e minimos do ponto (x[i], y[i], z[i])
+        // TODO: terminar o for externo
+        #pragma omp parallel for 
+        for (int i = start; i < end; i++) {
+            int min_manhattan_local = INT_MAX, max_manhattan_local = INT_MIN;
+            double min_euclidean_local = INT_MAX, max_euclidean_local = INT_MIN;
+
+            // obtém as distâncias de manhattan e euclidiana entre o ponto (x[i], y[i], z[i]) e todos os outros pontos
+            #pragma omp parallel for reduction(min: min_manhattan_local, min_euclidean_local) reduction(max: max_manhattan_local, max_euclidean_local)
+            for (int j = i + 1; j < N * N; j++) {
+                // manhattan_distance e euclidean_distance são variáveis locais de cada thread
+                int manhattan_distance = manhattan(x[i], y[i], z[i], x[j], y[j], z[j]);
+                double euclidean_distance = euclidian(x[i], y[i], z[i], x[j], y[j], z[j]);
+
+                // atualiza os valores máximos e mínimos
+                if (manhattan_distance < min_manhattan_local) {
+                    min_manhattan_local = manhattan_distance;
+                }
+
+                if (manhattan_distance > max_manhattan_local) {
+                    max_manhattan_local = manhattan_distance;
+                }
+
+                if (euclidean_distance < min_euclidean_local) {
+                    min_euclidean_local = euclidean_distance;
+                }
+
+                if (euclidean_distance > max_euclidean_local) {
+                    max_euclidean_local = euclidean_distance;
+                }
+            } // end for j
+        } // end for i
+        
+
+        // envia os valores máximos e mínimos para o processo 0
+
+
+
+
+
+    } // end else
 
     free(x);
     free(y);
     free(z);
 
+    // Encerra o MPI
     if (MPI_Finalize() != MPI_SUCCESS) {
         printf("Error on MPI_Finalize()\n");
         return EXIT_FAILURE;
@@ -133,9 +179,4 @@ void printMatrix(int *matrix, int row, int column) {
     }
 
     printf("\n");
-}
-
-// Libera a memória alocada para a matriz
-void freeMatrix(int *matrix, int row) {
-    free(matrix);
 }
